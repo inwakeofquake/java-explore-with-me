617ae64e1ab95f6fd50cc07484bcd984b6d40931
inwakeofquake <ku7min@yandex.ru>
2023-08-12 18:59:46 +0300

    rollback: cut unnecessary validations in UpdateEventUserDto fix: added more @NotBlank validations to more dto classes

--- main-service/src/main/java/ru/practicum/main_service/dto/category/CategoryResponseDto.java
+++ main-service/src/main/java/ru/practicum/main_service/dto/category/CategoryResponseDto.java
@@ -4,10 +4,13 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 @Builder
 public class CategoryResponseDto {
     private Long id;
+    @NotBlank
     private String name;
 }
